294063a7317fa6d032c1ea36f7eab0bd93148e80
Lenin <leninbose1596@gmail.com>
2024-01-03 20:54:54 +0530

    added checkbalance func. to js to display on frontend.

--- src/dbank_frontend/src/index.js
+++ src/dbank_frontend/src/index.js
@@ -1,5 +1,7 @@
 import { dbank_backend } from "../../declarations/dbank_backend";
 
-window.addEventListener("load", () => {
-  console.log("Hello, WebAssembly!");
+window.addEventListener("load", async () => {
+  // console.log("Hello, WebAssembly!");
+  const currentAmount = await dbank_backend.checkBalance();
+  document.getElementById("value").innerText = currentAmount.toFixed(2);
 });
